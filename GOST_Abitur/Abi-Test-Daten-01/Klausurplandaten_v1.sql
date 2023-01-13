ced1e321d0ec8e0ebe9d857c45358bc25f7c320c
ESR <mail@esser5.de>
2023-01-13 19:39:11 +0100

    Klausurplandaten v1
    - Vorgaben

---
+++ GOST_Abitur/Abi-Test-Daten-01/Klausurplandaten_v1.sql
@@ -0,0 +1,397 @@
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (0,-1,5,71,'GK',1,180,30,0,0,0,NULL),
+	 (1,-1,2,16,'LK',1,180,0,0,0,0,NULL),
+	 (2,-1,2,16,'LK',2,180,0,0,0,0,NULL),
+	 (3,-1,3,16,'LK',1,180,0,0,0,0,NULL),
+	 (4,-1,3,16,'LK',2,180,0,0,0,0,NULL),
+	 (5,-1,4,16,'LK',1,225,0,0,0,0,NULL),
+	 (6,-1,4,16,'LK',2,225,0,0,0,0,NULL),
+	 (8,-1,5,16,'LK',1,255,30,0,0,0,NULL),
+	 (9,-1,5,16,'LK',2,255,30,0,0,0,NULL),
+	 (10,-1,0,16,'GK',1,90,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (11,-1,0,16,'GK',2,90,0,0,0,0,NULL),
+	 (12,-1,1,16,'GK',1,90,0,0,0,0,NULL),
+	 (13,-1,1,16,'GK',2,100,0,0,0,0,'Vergleichsklausur'),
+	 (14,-1,2,16,'GK',1,135,0,0,0,0,NULL),
+	 (15,-1,2,16,'GK',2,135,0,0,0,0,NULL),
+	 (16,-1,3,16,'GK',1,135,0,0,0,0,NULL),
+	 (17,-1,3,16,'GK',2,135,0,0,0,0,NULL),
+	 (18,-1,4,16,'GK',1,135,0,0,0,0,NULL),
+	 (19,-1,4,16,'GK',2,135,0,0,0,0,NULL),
+	 (21,-1,5,16,'GK',1,180,30,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (22,-1,5,16,'GK',2,180,30,0,0,0,NULL),
+	 (23,-1,2,17,'LK',1,180,0,0,0,0,NULL),
+	 (25,-1,3,17,'LK',1,180,0,1,1,0,NULL),
+	 (26,-1,3,17,'LK',2,180,0,0,0,0,NULL),
+	 (27,-1,4,17,'LK',1,225,0,1,1,0,NULL),
+	 (28,-1,4,17,'LK',2,225,0,0,0,0,NULL),
+	 (29,-1,5,17,'LK',1,255,30,0,0,0,NULL),
+	 (30,-1,5,17,'LK',2,255,30,0,0,0,NULL),
+	 (31,-1,0,17,'GK',1,90,0,1,1,0,NULL),
+	 (32,-1,0,17,'GK',2,90,0,0,0,0,'evtl. wenige wegen mündl. Prüfung 
+E/F');
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (33,-1,1,17,'GK',1,90,0,0,0,0,NULL),
+	 (34,-1,1,17,'GK',2,90,0,0,0,0,NULL),
+	 (35,-1,2,17,'GK',1,135,0,1,1,0,NULL),
+	 (37,-1,3,17,'GK',1,135,0,0,0,0,NULL),
+	 (38,-1,3,17,'GK',2,135,0,0,0,0,NULL),
+	 (39,-1,4,17,'GK',1,135,0,1,1,0,NULL),
+	 (40,-1,4,17,'GK',2,135,0,0,0,0,NULL),
+	 (41,-1,5,17,'GK',1,180,30,0,0,0,NULL),
+	 (42,-1,5,17,'GK',2,180,30,0,0,0,NULL),
+	 (43,-1,2,30,'LK',1,180,0,1,1,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (45,-1,3,30,'LK',1,180,0,0,0,0,NULL),
+	 (46,-1,3,30,'LK',2,180,0,0,0,0,NULL),
+	 (47,-1,4,30,'LK',1,225,0,0,0,0,NULL),
+	 (48,-1,4,30,'LK',2,225,0,1,0,0,NULL),
+	 (49,-1,5,30,'LK',1,255,30,0,0,0,NULL),
+	 (50,-1,5,30,'LK',2,255,30,0,0,0,NULL),
+	 (51,-1,0,30,'GK',1,90,0,1,1,0,NULL),
+	 (52,-1,0,30,'GK',2,90,0,0,0,0,'evtl. wenige wegen mündl. Prüfung 
+E/F'),
+	 (53,-1,1,30,'GK',1,90,0,0,0,0,NULL),
+	 (54,-1,1,30,'GK',2,90,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (55,-1,2,30,'GK',1,135,0,1,1,0,NULL),
+	 (57,-1,3,30,'GK',1,135,0,0,0,0,NULL),
+	 (58,-1,3,30,'GK',2,135,0,0,0,0,NULL),
+	 (59,-1,4,30,'GK',1,135,0,0,0,0,NULL),
+	 (60,-1,4,30,'GK',2,135,0,1,1,0,NULL),
+	 (61,-1,5,30,'GK',1,180,30,0,0,0,NULL),
+	 (62,-1,5,30,'GK',2,180,30,0,0,0,NULL),
+	 (63,-1,2,72,'LK',1,180,0,0,0,0,NULL),
+	 (64,-1,2,72,'LK',2,180,0,0,0,0,NULL),
+	 (65,-1,3,72,'LK',1,180,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (66,-1,3,72,'LK',2,180,0,0,0,0,NULL),
+	 (67,-1,4,72,'LK',1,225,0,0,0,0,NULL),
+	 (68,-1,4,72,'LK',2,225,0,0,0,0,NULL),
+	 (69,-1,5,72,'LK',1,255,30,0,0,0,NULL),
+	 (70,-1,5,72,'LK',2,255,30,0,0,0,NULL),
+	 (71,-1,0,72,'GK',1,90,0,0,0,0,NULL),
+	 (72,-1,0,72,'GK',2,90,0,0,0,0,NULL),
+	 (73,-1,1,72,'GK',1,90,0,0,0,0,NULL),
+	 (74,-1,1,72,'GK',2,90,0,0,0,0,NULL),
+	 (75,-1,2,72,'GK',1,135,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (76,-1,2,72,'GK',2,135,0,0,0,0,NULL),
+	 (77,-1,3,72,'GK',1,135,0,0,0,0,NULL),
+	 (78,-1,3,72,'GK',2,135,0,0,0,0,NULL),
+	 (79,-1,4,72,'GK',1,135,0,0,0,0,NULL),
+	 (80,-1,4,72,'GK',2,135,0,0,0,0,NULL),
+	 (81,-1,5,72,'GK',1,180,30,0,0,0,NULL),
+	 (82,-1,5,72,'GK',2,180,45,0,0,0,NULL),
+	 (95,-1,0,223,'GK',1,90,0,1,1,0,NULL),
+	 (96,-1,0,223,'GK',2,90,0,1,1,0,NULL),
+	 (97,-1,1,223,'GK',1,90,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (98,-1,1,223,'GK',2,90,0,0,0,0,NULL),
+	 (99,-1,2,223,'GK',1,135,0,0,0,0,NULL),
+	 (101,-1,3,223,'GK',1,135,0,1,1,0,NULL),
+	 (102,-1,3,223,'GK',2,135,0,0,0,0,NULL),
+	 (103,-1,4,223,'GK',1,135,0,1,1,0,NULL),
+	 (104,-1,4,223,'GK',2,135,0,0,0,0,NULL),
+	 (105,-1,5,223,'GK',1,180,30,0,0,0,NULL),
+	 (106,-1,5,223,'GK',2,180,30,0,0,0,NULL),
+	 (108,-1,0,81,'GK',2,90,0,0,0,0,NULL),
+	 (109,-1,1,81,'GK',1,90,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (111,-1,2,81,'GK',1,135,0,0,0,0,NULL),
+	 (112,-1,2,81,'GK',2,135,0,0,0,0,NULL),
+	 (113,-1,3,81,'GK',1,135,0,0,0,0,NULL),
+	 (114,-1,3,81,'GK',2,135,0,0,0,0,NULL),
+	 (115,-1,4,81,'GK',1,135,0,0,0,0,NULL),
+	 (116,-1,4,81,'GK',2,135,0,0,0,0,NULL),
+	 (117,-1,5,81,'GK',1,180,30,0,0,0,NULL),
+	 (118,-1,5,81,'GK',2,180,30,0,0,0,NULL),
+	 (120,-1,0,71,'GK',2,90,0,0,0,0,NULL),
+	 (121,-1,1,71,'GK',1,90,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (123,-1,2,71,'GK',1,135,0,0,0,0,NULL),
+	 (124,-1,2,71,'GK',2,180,0,0,0,0,NULL),
+	 (125,-1,3,71,'GK',1,135,0,0,0,0,NULL),
+	 (126,-1,3,71,'GK',2,135,0,0,0,0,NULL),
+	 (127,-1,4,71,'GK',1,135,0,0,0,0,NULL),
+	 (128,-1,4,71,'GK',2,135,0,0,0,0,NULL),
+	 (130,-1,5,71,'GK',2,180,30,0,0,0,NULL),
+	 (133,-1,2,37,'LK',1,180,0,0,0,0,NULL),
+	 (134,-1,2,37,'LK',2,180,0,0,0,0,NULL),
+	 (135,-1,3,37,'LK',1,180,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (136,-1,3,37,'LK',2,180,0,0,0,0,NULL),
+	 (138,-1,4,37,'LK',1,180,0,0,0,0,NULL),
+	 (139,-1,4,37,'LK',2,180,0,0,0,0,NULL),
+	 (140,-1,5,37,'LK',1,255,30,0,0,0,NULL),
+	 (141,-1,5,37,'LK',2,255,30,0,0,0,NULL),
+	 (143,-1,0,37,'GK',2,90,0,0,0,0,NULL),
+	 (144,-1,1,37,'GK',1,90,0,0,0,0,NULL),
+	 (146,-1,2,37,'GK',1,135,0,0,0,0,NULL),
+	 (147,-1,2,37,'GK',2,135,0,0,0,0,NULL),
+	 (148,-1,3,37,'GK',1,135,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (149,-1,3,37,'GK',2,135,0,0,0,0,NULL),
+	 (150,-1,4,37,'GK',1,135,0,0,0,0,NULL),
+	 (151,-1,4,37,'GK',2,135,0,0,0,0,NULL),
+	 (152,-1,5,37,'GK',1,180,30,0,0,0,NULL),
+	 (153,-1,5,37,'GK',2,180,30,0,0,0,NULL),
+	 (154,-1,2,24,'LK',1,180,0,0,0,0,NULL),
+	 (155,-1,2,24,'LK',2,180,0,0,0,0,NULL),
+	 (156,-1,3,24,'LK',1,180,0,0,0,0,NULL),
+	 (157,-1,3,24,'LK',2,180,0,0,0,0,NULL),
+	 (158,-1,4,24,'LK',1,225,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (159,-1,4,24,'LK',2,225,0,0,0,0,NULL),
+	 (160,-1,5,24,'LK',1,255,30,0,0,0,NULL),
+	 (161,-1,5,24,'LK',2,255,30,0,0,0,NULL),
+	 (163,-1,0,24,'GK',2,90,0,0,0,0,NULL),
+	 (164,-1,1,24,'GK',1,90,0,0,0,0,NULL),
+	 (165,-1,2,24,'GK',1,135,0,0,0,0,NULL),
+	 (166,-1,2,24,'GK',2,135,0,0,0,0,NULL),
+	 (167,-1,3,24,'GK',1,135,0,0,0,0,NULL),
+	 (168,-1,3,24,'GK',2,135,0,0,0,0,NULL),
+	 (169,-1,4,24,'GK',1,135,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (170,-1,4,24,'GK',2,135,0,0,0,0,NULL),
+	 (171,-1,5,24,'GK',1,180,30,0,0,0,NULL),
+	 (172,-1,5,24,'GK',2,180,30,0,0,0,NULL),
+	 (173,-1,2,164,'LK',1,180,0,0,0,0,NULL),
+	 (174,-1,2,164,'LK',2,180,0,0,0,0,NULL),
+	 (175,-1,3,164,'LK',1,180,0,0,0,0,NULL),
+	 (176,-1,3,164,'LK',2,180,0,0,0,0,NULL),
+	 (177,-1,4,164,'LK',1,225,0,0,0,0,NULL),
+	 (178,-1,4,164,'LK',2,225,0,0,0,0,NULL),
+	 (179,-1,5,164,'LK',1,255,30,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (180,-1,5,164,'LK',2,255,30,0,0,0,NULL),
+	 (181,-1,0,164,'GK',2,90,0,0,0,0,NULL),
+	 (182,-1,1,164,'GK',1,90,0,0,0,0,NULL),
+	 (183,-1,2,164,'GK',1,135,0,0,0,0,NULL),
+	 (184,-1,2,164,'GK',2,135,0,0,0,0,NULL),
+	 (185,-1,3,164,'GK',1,135,0,0,0,0,NULL),
+	 (186,-1,3,164,'GK',2,135,0,0,0,0,NULL),
+	 (187,-1,4,164,'GK',1,135,0,0,0,0,NULL),
+	 (188,-1,4,164,'GK',2,135,0,0,0,0,NULL),
+	 (189,-1,5,164,'GK',1,180,30,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (190,-1,5,164,'GK',2,180,30,0,0,0,NULL),
+	 (191,-1,2,26,'LK',1,180,0,0,0,0,NULL),
+	 (192,-1,2,26,'LK',2,180,0,0,0,0,NULL),
+	 (193,-1,3,26,'LK',1,180,0,0,0,0,NULL),
+	 (194,-1,3,26,'LK',2,180,0,0,0,0,NULL),
+	 (195,-1,4,26,'LK',1,225,0,0,0,0,NULL),
+	 (196,-1,4,26,'LK',2,225,0,0,0,0,NULL),
+	 (197,-1,5,26,'LK',1,255,30,0,0,0,NULL),
+	 (198,-1,5,26,'LK',2,255,30,0,0,0,NULL),
+	 (199,-1,0,26,'GK',2,90,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (200,-1,1,26,'GK',1,90,0,0,0,0,NULL),
+	 (201,-1,2,26,'GK',1,135,0,0,0,0,NULL),
+	 (202,-1,2,26,'GK',2,135,0,0,0,0,NULL),
+	 (203,-1,3,26,'GK',1,135,0,0,0,0,NULL),
+	 (204,-1,3,26,'GK',2,135,0,0,0,0,NULL),
+	 (205,-1,4,26,'GK',1,135,0,0,0,0,NULL),
+	 (206,-1,4,26,'GK',2,135,0,0,0,0,NULL),
+	 (207,-1,5,26,'GK',1,180,30,0,0,0,NULL),
+	 (208,-1,5,26,'GK',2,180,30,0,0,0,NULL),
+	 (209,-1,0,127,'GK',2,90,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (210,-1,1,127,'GK',1,90,0,0,0,0,NULL),
+	 (211,-1,2,127,'GK',1,135,0,0,0,0,NULL),
+	 (212,-1,2,127,'GK',2,135,0,0,0,0,NULL),
+	 (213,-1,3,127,'GK',1,135,0,0,0,0,NULL),
+	 (214,-1,3,127,'GK',2,135,0,0,0,0,NULL),
+	 (215,-1,4,127,'GK',1,135,0,0,0,0,NULL),
+	 (216,-1,4,127,'GK',2,135,0,0,0,0,NULL),
+	 (217,-1,5,127,'GK',1,180,30,0,0,0,NULL),
+	 (218,-1,5,127,'GK',2,180,30,0,0,0,NULL),
+	 (219,-1,2,79,'LK',1,180,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (220,-1,2,79,'LK',2,180,0,0,0,0,NULL),
+	 (221,-1,3,79,'LK',1,180,0,0,0,0,NULL),
+	 (222,-1,3,79,'LK',2,180,0,0,0,0,NULL),
+	 (223,-1,4,79,'LK',1,225,0,0,0,0,NULL),
+	 (224,-1,4,79,'LK',2,225,0,0,0,0,NULL),
+	 (225,-1,5,79,'LK',1,255,0,0,0,0,NULL),
+	 (226,-1,5,79,'LK',2,255,0,0,0,0,NULL),
+	 (227,-1,0,79,'GK',1,90,0,0,0,0,NULL),
+	 (228,-1,0,79,'GK',2,90,0,0,0,0,NULL),
+	 (229,-1,1,79,'GK',1,90,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (230,-1,1,79,'GK',2,100,0,0,0,0,'Vergleichsklausur'),
+	 (231,-1,2,79,'GK',1,135,0,0,0,0,NULL),
+	 (232,-1,2,79,'GK',2,135,0,0,0,0,NULL),
+	 (233,-1,3,79,'GK',1,135,0,0,0,0,NULL),
+	 (234,-1,3,79,'GK',2,135,0,0,0,0,NULL),
+	 (235,-1,4,79,'GK',1,180,0,0,0,0,NULL),
+	 (236,-1,4,79,'GK',2,180,0,0,0,0,NULL),
+	 (237,-1,5,79,'GK',1,180,0,0,0,0,NULL),
+	 (238,-1,5,79,'GK',2,180,0,0,0,0,NULL),
+	 (239,-1,0,193,'GK',2,90,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (240,-1,1,193,'GK',1,90,0,0,0,0,NULL),
+	 (241,-1,2,193,'GK',1,135,0,0,0,0,NULL),
+	 (242,-1,2,193,'GK',2,135,0,0,0,0,NULL),
+	 (243,-1,3,193,'GK',1,135,0,0,0,0,NULL),
+	 (244,-1,3,193,'GK',2,135,0,0,0,0,NULL),
+	 (245,-1,4,193,'GK',1,135,0,0,0,0,NULL),
+	 (246,-1,4,193,'GK',2,135,0,0,0,0,NULL),
+	 (247,-1,5,193,'GK',1,180,0,0,0,0,NULL),
+	 (248,-1,5,193,'GK',2,180,0,0,0,0,NULL),
+	 (249,-1,2,192,'LK',1,180,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (250,-1,2,192,'LK',2,180,0,0,0,0,NULL),
+	 (251,-1,3,192,'LK',1,180,0,0,0,0,NULL),
+	 (252,-1,3,192,'LK',2,180,0,0,0,0,NULL),
+	 (253,-1,4,192,'LK',1,180,0,0,0,0,NULL),
+	 (254,-1,4,192,'LK',2,180,0,0,0,0,NULL),
+	 (255,-1,5,192,'LK',1,255,0,0,0,0,NULL),
+	 (256,-1,5,192,'LK',2,255,0,0,0,0,NULL),
+	 (257,-1,0,192,'GK',2,90,0,0,0,0,NULL),
+	 (258,-1,1,192,'GK',1,90,0,0,0,0,NULL),
+	 (259,-1,2,192,'GK',1,135,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (260,-1,2,192,'GK',2,135,0,0,0,0,NULL),
+	 (261,-1,3,192,'GK',1,135,0,0,0,0,NULL),
+	 (262,-1,3,192,'GK',2,135,0,0,0,0,NULL),
+	 (263,-1,4,192,'GK',1,135,0,0,0,0,NULL),
+	 (264,-1,4,192,'GK',2,135,0,0,0,0,NULL),
+	 (265,-1,5,192,'GK',1,180,0,0,0,0,NULL),
+	 (266,-1,5,192,'GK',2,180,0,0,0,0,NULL),
+	 (267,-1,2,128,'LK',1,135,0,0,0,0,NULL),
+	 (268,-1,2,128,'LK',2,135,0,0,0,0,NULL),
+	 (269,-1,3,128,'LK',1,135,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (270,-1,3,128,'LK',2,135,0,0,0,0,NULL),
+	 (271,-1,4,128,'LK',1,180,0,0,0,0,NULL),
+	 (272,-1,4,128,'LK',2,180,0,0,0,0,NULL),
+	 (273,-1,5,128,'LK',1,255,0,0,0,0,NULL),
+	 (274,-1,5,128,'LK',2,255,0,0,0,0,NULL),
+	 (275,-1,0,128,'GK',2,90,0,0,0,0,NULL),
+	 (276,-1,1,128,'GK',1,90,0,0,0,0,NULL),
+	 (277,-1,2,128,'GK',1,90,0,0,0,0,NULL),
+	 (278,-1,2,128,'GK',2,90,0,0,0,0,NULL),
+	 (279,-1,3,128,'GK',1,90,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (280,-1,3,128,'GK',2,90,0,0,0,0,NULL),
+	 (281,-1,4,128,'GK',1,135,0,0,0,0,NULL),
+	 (282,-1,4,128,'GK',2,135,0,0,0,0,NULL),
+	 (283,-1,5,128,'GK',1,180,0,0,0,0,NULL),
+	 (284,-1,5,128,'GK',2,180,0,0,0,0,NULL),
+	 (285,-1,2,8,'LK',1,135,0,0,0,0,NULL),
+	 (286,-1,2,8,'LK',2,135,0,0,0,0,NULL),
+	 (287,-1,3,8,'LK',1,135,0,0,0,0,NULL),
+	 (288,-1,3,8,'LK',2,135,0,0,0,0,NULL),
+	 (289,-1,4,8,'LK',1,180,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (290,-1,4,8,'LK',2,180,0,0,0,0,NULL),
+	 (291,-1,5,8,'LK',1,255,0,0,0,0,NULL),
+	 (292,-1,5,8,'LK',2,255,0,0,0,0,NULL),
+	 (293,-1,0,8,'GK',2,90,0,0,0,0,NULL),
+	 (294,-1,1,8,'GK',1,90,0,0,0,0,NULL),
+	 (295,-1,2,8,'GK',1,90,0,0,0,0,NULL),
+	 (296,-1,2,8,'GK',2,90,0,0,0,0,NULL),
+	 (297,-1,3,8,'GK',1,90,0,0,0,0,NULL),
+	 (298,-1,3,8,'GK',2,90,0,0,0,0,NULL),
+	 (299,-1,4,8,'GK',1,135,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (300,-1,4,8,'GK',2,135,0,0,0,0,NULL),
+	 (301,-1,5,8,'GK',1,180,0,0,0,0,NULL),
+	 (302,-1,5,8,'GK',2,180,0,0,0,0,NULL),
+	 (303,-1,0,70,'GK',2,90,0,0,0,0,NULL),
+	 (304,-1,1,70,'GK',1,90,0,0,0,0,NULL),
+	 (305,-1,2,70,'GK',1,135,0,0,0,0,NULL),
+	 (306,-1,2,70,'GK',2,135,0,0,0,0,NULL),
+	 (307,-1,3,70,'GK',1,135,0,0,0,0,NULL),
+	 (308,-1,3,70,'GK',2,135,0,0,0,0,NULL),
+	 (309,-1,4,70,'GK',1,135,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (310,-1,4,70,'GK',2,135,0,0,0,0,NULL),
+	 (311,-1,5,70,'GK',1,180,30,0,0,0,NULL),
+	 (312,-1,5,70,'GK',2,180,30,0,0,0,NULL),
+	 (313,-1,0,27,'GK',2,90,0,0,0,0,NULL),
+	 (314,-1,1,27,'GK',1,90,0,0,0,0,NULL),
+	 (315,-1,2,27,'GK',1,135,0,0,0,0,NULL),
+	 (316,-1,2,27,'GK',2,135,0,0,0,0,NULL),
+	 (317,-1,3,27,'GK',1,135,0,0,0,0,NULL),
+	 (318,-1,3,27,'GK',2,135,0,0,0,0,NULL),
+	 (319,-1,4,27,'GK',1,135,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (320,-1,4,27,'GK',2,135,0,0,0,0,NULL),
+	 (321,-1,5,27,'GK',1,180,30,0,0,0,NULL),
+	 (322,-1,5,27,'GK',2,180,30,0,0,0,NULL),
+	 (323,-1,2,193,'LK',1,180,0,0,0,0,NULL),
+	 (324,-1,2,193,'LK',2,180,0,0,0,0,NULL),
+	 (325,-1,3,193,'LK',1,180,0,0,0,0,NULL),
+	 (326,-1,3,193,'LK',2,180,0,0,0,0,NULL),
+	 (327,-1,4,193,'LK',1,180,0,0,0,0,NULL),
+	 (328,-1,4,193,'LK',2,180,0,0,0,0,NULL),
+	 (329,-1,5,193,'LK',1,255,0,0,0,0,NULL);
+INSERT INTO GymAbi.Gost_Klausuren_Vorgaben 
+(ID,Abi_Jahrgang,Halbjahr,Fach_ID,KursartAllg,Quartal,Dauer,Auswahlzeit,IstMdlPruefung,IstAudioNotwendig,IstVideoNotwendig,Bemerkungen) 
+VALUES
+	 (330,-1,5,193,'LK',2,255,0,0,0,0,NULL);
+
