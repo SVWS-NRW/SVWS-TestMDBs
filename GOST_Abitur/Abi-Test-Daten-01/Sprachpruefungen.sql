00b03926f5f29ddc1b846698e5767576f60c3034
hmt <dev@hmt.im>
2024-04-22 12:29:52 +0200

    Sprachprüfungen als SQL in in der GymAbi.sqlite ergänzt

---
+++ GOST_Abitur/Abi-Test-Daten-01/Sprachpruefungen.sql
@@ -0,0 +1,7 @@
+INSERT INTO SchuelerSprachpruefungen
+(ID, Schueler_ID, Sprache, ASDJahrgang, Anspruchsniveau_ID, Pruefungsdatum, ErsetzteSprache, IstHSUPruefung, IstFeststellungspruefung, KannErstePflichtfremdspracheErsetzen, KannZweitePflichtfremdspracheErsetzen, KannWahlpflichtfremdspracheErsetzen, KannBelegungAlsFortgefuehrteSpracheErlauben, Referenzniveau, NotePruefung)
+VALUES(1, 1199, 'TM', '09', 3, '2023-05-03', NULL, 1, 0, NULL, NULL, NULL, 1, 'B1', 2);
+INSERT INTO SchuelerSprachpruefungen
+(ID, Schueler_ID, Sprache, ASDJahrgang, Anspruchsniveau_ID, Pruefungsdatum, ErsetzteSprache, IstHSUPruefung, IstFeststellungspruefung, KannErstePflichtfremdspracheErsetzen, KannZweitePflichtfremdspracheErsetzen, KannWahlpflichtfremdspracheErsetzen, KannBelegungAlsFortgefuehrteSpracheErlauben, Referenzniveau, NotePruefung)
+VALUES(2, 1244, 'PM', '09', 3, '2023-04-29', NULL, 0, 1, NULL, 1, 1, NULL, 'B1', 1); 
+
